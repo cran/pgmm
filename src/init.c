f1f3ecca5fca87089f7a758c999745c4ac54c13b
Paul D. McNicholas <mcnicholas@math.mcmaster.ca>
2018-03-15 09:49:21 +0000

    version 1.2.1

---
+++ src/init.c
@@ -0,0 +1,20 @@
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* FIXME: 
+   Check these declarations against the C/Fortran source code.
+*/
+
+/* .C calls */
+extern void pgmm_c(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+
+static const R_CMethodDef CEntries[] = {
+    {"pgmm_c", (DL_FUNC) &pgmm_c, 13},
+    {NULL, NULL, 0}
+};
+
+void R_init_pgmm(DllInfo *dll)
+{
+    R_registerRoutines(dll, CEntries, NULL, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
